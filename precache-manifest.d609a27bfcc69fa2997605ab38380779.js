d3634eda4071d17968715626d7f64353a33c4667
Petro <locator862@gmail.com>
2022-06-09 16:04:40 +0200

    Updates

---
+++ precache-manifest.d609a27bfcc69fa2997605ab38380779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b76c41aa86442bdeee4c",
+    "url": "/GAME-sink-the-ship/static/css/main.000b1a7d.chunk.css"
+  },
+  {
+    "revision": "b76c41aa86442bdeee4c",
+    "url": "/GAME-sink-the-ship/static/js/main.b76c41aa.chunk.js"
+  },
+  {
+    "revision": "82458581948fda41f1ba",
+    "url": "/GAME-sink-the-ship/static/js/1.82458581.chunk.js"
+  },
+  {
+    "revision": "39737cf0df1383cf33ae",
+    "url": "/GAME-sink-the-ship/static/js/runtime~main.39737cf0.js"
+  },
+  {
+    "revision": "2eb9d7ca0b6543ea278a35366840e738",
+    "url": "/GAME-sink-the-ship/index.html"
+  }
+];
